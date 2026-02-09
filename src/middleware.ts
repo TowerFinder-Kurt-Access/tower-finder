f11936e15c5a0aa52dee90711298dbff5408e92e
alejandroem <alexalejandroem@gmail.com>
2026-02-09 05:27:12 -0300

    fix in logic for middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,26 +5,23 @@ import { authConfig } from '@/lib/auth.config';
 const { auth } = NextAuth(authConfig);
 
 export default auth((req: any) => {
+  const isLoggedIn = !!req.auth?.user;
   const { pathname } = req.nextUrl;
 
-  const isLoggedIn = !!req.auth?.user;
+  if (pathname.startsWith('/api/auth') || pathname === '/login') {
+    if (isLoggedIn && pathname === '/login') {
+      return Response.redirect(new URL('/', req.url));
+    }
+    return;
+  }
 
   if (!isLoggedIn) {
     const loginUrl = new URL('/login', req.url);
     loginUrl.searchParams.set('callbackUrl', pathname);
     return Response.redirect(loginUrl);
-  } else {
-    if (isLoggedIn && pathname === '/login') {
-      return Response.redirect(new URL('/', req.url));
-    }
-    if (pathname.startsWith('/login') || pathname.startsWith('/api/auth')) {
-      return;
-    }
   }
 });
 
 export const config = {
-  matcher: [
-    '/((?!_next/static|_next/image|favicon.ico).*)',
-  ],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 };
